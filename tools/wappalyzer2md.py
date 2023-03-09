0de28f2050d83a51d4af0b085e337034483ba545
RawMain121 <freekali@protonmail.com>
2023-03-09 15:35:01 +0100

    wappalyzer2md.py update

--- tools/wappalyzer2md.py
+++ tools/wappalyzer2md.py
@@ -1,9 +1,11 @@
 import csv
 import argparse
+import os
 
 # Define arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--input', help='Input CSV file')
+parser = argparse.ArgumentParser(description='Convert Wappalyzer CSV file to Markdown format')
+parser.add_argument('-i', '--input', help='Input CSV file', required=True)
+parser.add_argument('-o', '--output', help='Output Markdown file')
 args = parser.parse_args()
 
 # Read CSV file
@@ -22,8 +24,16 @@ for i, header in enumerate(headers):
 headers = [header for i, header in enumerate(headers) if i not in empty_columns]
 data = [[cell for i, cell in enumerate(row) if i not in empty_columns] for row in data]
 
+# Determine output file name
+if args.output:
+    output_filename = args.output
+else:
+    url_index = headers.index('URL') if 'URL' in headers else 0
+    url_value = data[0][url_index].replace('http://', '').replace('https://', '').replace('/', '-').replace(':', '-') + '.md'
+    output_filename = "Wappalyzer2MD_" + url_value 
+
 # Write to Markdown file
-with open('output.md', 'w') as outfile:
+with open(output_filename, 'w') as outfile:
     # Write headers
     outfile.write('| ' + ' | '.join(headers) + ' |\n')
     outfile.write('| ' + ' | '.join(['---' for header in headers]) + ' |\n')
